bd86f3a71259cd9dbb029d96409a73d034a2b750
alex <alex@8a740191-43b4-49c3-93ab-3d0e180552ad>
2011-04-09 15:42:04 +0000

    fix for error in rss
    git-svn-id: https://svn.hartcode.com/internal/java/hartcode.com/branches/v1.4.0.0@83 8a740191-43b4-49c3-93ab-3d0e180552ad

--- src/servlets/RSSNewsPage.java
+++ src/servlets/RSSNewsPage.java
@@ -1,6 +1,8 @@
 package servlets;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import modules.RSSModule;
 import pages.RSSPage;
 
@@ -17,11 +19,10 @@ public class RSSNewsPage extends ServletPage {
     public RSSNewsPage() {
         super("application/rss+xml");
     }
-
-    public String GetFileData()
-    {
-		RSSPage rp = new RSSPage(new RSSModule(),"HartCode News","A forum for me to share my thoughts, programs, code, drawings, and other random ideas with the on-line community.","http://www.hartcode.com/");
-		return rp.toString();
-    }
     
+    public String GetFileData(HttpServletRequest request)
+    {
+    	RSSPage rp = new RSSPage(new RSSModule(),"HartCode News","A forum for me to share my thoughts, programs, code, drawings, and other random ideas with the on-line community.","http://www.hartcode.com/");
+    	return rp.toString();
+    }
 }
