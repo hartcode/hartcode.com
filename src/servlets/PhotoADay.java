a88c9f8720bec450d15db476cf743d707762645e
alex <alex@8a740191-43b4-49c3-93ab-3d0e180552ad>
2012-01-04 04:18:52 +0000

    git-svn-id: https://svn.hartcode.com/internal/java/hartcode.com/branches/v1.4.0.0@341 8a740191-43b4-49c3-93ab-3d0e180552ad

--- src/servlets/PhotoADay.java
+++ src/servlets/PhotoADay.java
@@ -2,12 +2,16 @@ package servlets;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.hartcode.PhotoADay.PhotoAdder;
 
 /**
  * Servlet implementation class PhotoADay
@@ -25,9 +29,29 @@ public class PhotoADay extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String strdate = request.getParameter("date");
+		if (strdate != null && strdate != "")
+		{
+			
+		
+		Date thedate = Date.valueOf(strdate);
+		Integer PhotoID = null;
+		String filename = null;
+		try {
+			PhotoID = PhotoAdder.GetPhotoDay(thedate);
+			filename = PhotoAdder.GetPhotoFileName(PhotoID);
+		} catch (Exception e) {
+			PrintWriter pw = response.getWriter();
+			//pw.write(thedate.toString());
+			//pw.write(PhotoID);
+			pw.write("exception: " + e.getMessage());
+			pw.close(); 
+		}
+		if (filename != null)
+		{
 		ServletOutputStream os = response.getOutputStream();
 		//FileInputStream is = new FileInputStream("/usr/java/tomcat-5.5/hartcode/ROOT/images/photos/1.jpg");
-		FileInputStream is = new FileInputStream("/home/photoaday/2.jpg");
+		FileInputStream is = new FileInputStream(filename);
 		int b = is.read();
 		while(b!= -1)
 		{
@@ -36,7 +60,21 @@ public class PhotoADay extends HttpServlet {
 		}
 		is.close();
 		os.close();
+		}else
+		{
+			PrintWriter pw = response.getWriter();
+			pw.write("filename was not found");
+			pw.close();
+		}
+		}
+		else
+		{
+			PrintWriter pw = response.getWriter();
+			pw.write("strdate was not found");
+			pw.close();
+		}
 	}
+	
 	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
