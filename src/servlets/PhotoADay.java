ee11706b95ef843a782559165b7e33795c5d5b34
alex <alex@8a740191-43b4-49c3-93ab-3d0e180552ad>
2012-01-04 21:42:33 +0000

    git-svn-id: https://svn.hartcode.com/internal/java/hartcode.com/branches/v1.4.0.0@348 8a740191-43b4-49c3-93ab-3d0e180552ad

--- src/servlets/PhotoADay.java
+++ src/servlets/PhotoADay.java
@@ -34,66 +34,63 @@ public class PhotoADay extends HttpServlet {
 		String strdate = request.getParameter("date");
 		if (strdate != null && strdate != "")
 		{
-			
-		Calendar mycal = Calendar.getInstance();
+			Calendar mycal = Calendar.getInstance();
+			java.util.Date thedate = Date.valueOf(strdate);
+			java.util.Date startdate = Date.valueOf("2012-01-01");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			java.util.Date stopdate = mycal.getTime();
+			String strdate2 = sdf.format(stopdate);
+			stopdate = Date.valueOf(strdate2);
 		
-		java.util.Date thedate = Date.valueOf(strdate);
-		java.util.Date startdate = Date.valueOf("2012-01-01");
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		java.util.Date stopdate = mycal.getTime();
-		String strdate2 = sdf.format(stopdate);
-		stopdate = Date.valueOf(strdate2);
-		
-		if (thedate.after(startdate) || thedate.equals(startdate))
-		{
-		if (thedate.before(stopdate) || thedate.equals(stopdate))
-		{
-		Integer PhotoID = null;
-		String filename = null;
-		try {
-			PhotoID = PhotoAdder.GetPhotoDay(thedate);
-			filename = PhotoAdder.GetPhotoFileName(PhotoID);
-		} catch (Exception e) {
-
-		}
-		if (filename != null)
-		{
-		ServletOutputStream os = response.getOutputStream();
-		//FileInputStream is = new FileInputStream("/usr/java/tomcat-5.5/hartcode/ROOT/images/photos/1.jpg");
-		FileInputStream is = new FileInputStream(filename);
-		int b = is.read();
-		while(b!= -1)
-		{
-			os.write(b);
-			b = is.read();
-		}
-		is.close();
-		os.close();
-		}else
-		{
-			PrintWriter pw = response.getWriter();
-			pw.write("filename was not found");
-			pw.close();
-		}
+			if (thedate.after(startdate) || thedate.equals(startdate))
+			{
+				if (thedate.before(stopdate) || thedate.equals(stopdate))
+				{
+					Integer PhotoID = null;
+					String filename = null;
+					try {
+						PhotoID = PhotoAdder.GetPhotoDay(thedate);
+						filename = PhotoAdder.GetPhotoFileName(PhotoID);
+					} catch (Exception e) {
+					}
+					if (filename != null)
+					{
+						ServletOutputStream os = response.getOutputStream();
+						//	FileInputStream is = new FileInputStream("/usr/java/tomcat-5.5/hartcode/ROOT/images/photos/1.jpg");
+						FileInputStream is = new FileInputStream(filename);
+						int b = is.read();
+						while(b!= -1)
+						{
+							os.write(b);
+							b = is.read();
+						}
+						is.close();
+						os.close();
+					}else
+					{
+						PrintWriter pw = response.getWriter();
+						pw.write("filename was not found");
+						pw.close();
+					}
+				}
+				else
+				{
+					PrintWriter pw = response.getWriter();
+					pw.write("date to late");	
+					pw.close();
+				}
+			}
+			else
+			{
+				PrintWriter pw = response.getWriter();
+				pw.write("date to early");
+				pw.close();
+			}
 		}
 		else
 		{
 			PrintWriter pw = response.getWriter();
 			pw.write("strdate was not found");
-			pw.close();
-		}
-		}
-		else
-		{
-			PrintWriter pw = response.getWriter();
-			pw.write("date to late");
-			pw.close();
-		}
-		}
-		else
-		{
-			PrintWriter pw = response.getWriter();
-			pw.write("date to early");
 			pw.close();
 		}
 	}
