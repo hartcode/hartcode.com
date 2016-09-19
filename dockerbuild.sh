0710d30fc2f0c696f20dfb6abae93cf62ea86bf2
Alexander Hart <alex@hartcode.com>
2016-09-19 21:07:42 +0000

    Delete dockerbuild.sh

--- dockerbuild.sh
+++
@@ -1,6 +0,0 @@
-IMAGENAME=gcr.io/hartonline-cloud/hartcode.com
-IMAGEVERSION=1.0
-docker build -t $IMAGENAME:$IMAGEVERSION .
-docker tag -f $IMAGENAME:$IMAGEVERSION $IMAGENAME:latest
-gcloud docker push $IMAGENAME:$IMAGEVERSION
-gcloud docker push $IMAGENAME:latest
