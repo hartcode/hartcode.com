aae8e0eac97120cc9c1385a6454e2d689f4010a4
Alex Hart <alex@hartcode.com>
2016-09-20 15:54:34 -0500

    [WIP] Configuring Webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,75 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'bootstrap/dist/js/bootstrap.min.js',
+        'jquery/dist/jquery.min.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modulesDirectories: ['src', 'node_modules']
+    },
+    plugins: [
+        new ExtractTextPlugin('css/bundle.css'),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            favicon: 'src/favicon.ico',
+            inject: false,
+            hash: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'url?limit=10000&hash=sha512&digest=hex&name=images/[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
+            }
+
+        ]
+    },
+    postcss() {
+      return [
+        require('postcss-import')({ addDependencyTo: webpack }),
+        require('postcss-url')(),
+        require('precss')({'import': {disable: true}}),
+        require('autoprefixer')({ browsers: ['last 2 versions'] })
+      ];
+    }
+
+};
