c9f43d4895088a6c2fca40c3b4f6f4968dc37fab
Alex Hart <alex@hartcode.com>
2016-09-26 12:31:42 -0500

    [WIP] Moving to ejs templates
    Setup the views for redesigning the website.

--- devServer.js
+++ devServer.js
@@ -1,7 +1,40 @@
 var express = require('express');
-    serveStatic = require('serve-static');
-
 var app = express();
 
-app.use(serveStatic("./src"));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/products', function(req, res) {
+  res.render('products');
+});
+
+app.get('/hire', function(req, res) {
+  res.render('hire');
+});
+
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
+app.get('/careers', function(req, res) {
+  res.render('careers');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact');
+});
+
+app.get('/privacy', function(req, res) {
+  res.render('privacy');
+});
+
+app.get('/terms', function(req, res) {
+  res.render('terms');
+});
+
+app.use(express.static(__dirname + '/html'));
+
 app.listen(8811);
