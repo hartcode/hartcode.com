80ed2b9bcb9e4bf218e6c6227e585cf4102f7779
Alex Hart <alex@hartcode.com>
2016-10-05 15:50:04 -0500

    Add github to about and footer pages.

--- src/client/client.js
+++ src/client/client.js
@@ -1,12 +1,20 @@
-import '../../node_modules/jquery/src/jquery.js'
+// css
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './css/main.css';
 import './css/navbar.css';
+
+// js dependencies
+import '../../node_modules/jquery/src/jquery.js'
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 document.getElementsByName('facebook').forEach(function(img){
  img.src = require('./images/facebook.png');});
 document.getElementsByName('twitter').forEach(function(img){
  img.src = require('./images/twitter.png');});
+document.getElementsByName('github').forEach(function(img){
+ img.src = require('./images/github.png');});
+document.getElementsByName('githublight').forEach(function(img){
+ img.src = require('./images/githublight.png');});
 document.getElementsByName('logo').forEach(function(img){
  img.src = require('./images/logo.png');});
 document.getElementsByName('alex').forEach(function(img){
