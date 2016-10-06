2f773d901ed11105860b4162b2fd18c031771d60
Alex Hart <alex@hartcode.com>
2016-10-06 13:44:20 -0500

    Remove unnecessary ejs views
    In moving to react I was able to combine all of the views into a single
    view main.ejs.

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,65 +1,74 @@
 module.exports = function(app) {
 
 app.get('/', function(req, res) {
-  res.render('index', {
+  res.render('main', {
     title: "",
-    bodyClass: "index"
+    bodyClass: "index",
+    jsFile:"index.js"
   });
 });
 
 app.get('/products', function(req, res) {
-  res.render('products', {
+  res.render('main', {
     title: "Products",
-    bodyClass: 'products'
+    bodyClass: 'products',
+    jsFile:"products.js"
   });
 });
 
 app.get('/hire', function(req, res) {
-  res.render('hire', {
+  res.render('main', {
     title: "Hire Us",
-    bodyClass: "hire"
+    bodyClass: "hire",
+    jsFile:"hire.js"
   });
 });
 
 app.get('/about', function(req, res) {
-  res.render('about', {
+  res.render('main', {
     title: "About Us",
-    bodyClass: "about"
+    bodyClass: "about",
+    jsFile:"about.js"
   });
 });
 
 app.get('/careers', function(req, res) {
-  res.render('careers', {
+  res.render('main', {
     title: "Careers",
-    bodyClass: "careers"
+    bodyClass: "careers",
+    jsFile:"careers.js"
   });
 });
 
 app.get('/contact', function(req, res) {
-  res.render('contact', {
+  res.render('main', {
     title: "Contact Us",
-    bodyClass: "contact"
+    bodyClass: "contact",
+    jsFile:"contact.js"
   });
 });
 
 app.get('/privacy', function(req, res) {
-  res.render('privacy', {
+  res.render('main', {
     title: "Privacy Policy",
-    bodyClass: "privacy"
+    bodyClass: "privacy",
+    jsFile:"privacy.js"
   });
 });
 
 app.get('/terms-of-use', function(req, res) {
-  res.render('terms-of-use', {
+  res.render('main', {
     title: "Terms of Use",
-    bodyClass: "terms-of-use"
+    bodyClass: "terms-of-use",
+    jsFile:"termsofuse.js"
   });
 });
 
 app.get('/end-user-license-agreement', function(req, res) {
-  res.render('end-user-license-agreement', {
+  res.render('main', {
     title: "End User License Agreement",
-    bodyClass: "end-user-license-agreement"
+    bodyClass: "end-user-license-agreement",
+    jsFile:"enduserlicenseagreement.js"
   });
 });
 }
