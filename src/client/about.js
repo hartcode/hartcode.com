fca1558cf40006589dfe553f8c50d1fccdb62846
Alex Hart <alex@hartcode.com>
2017-01-04 12:31:15 -0600

    Add Google Analytics

--- src/client/about.js
+++ src/client/about.js
@@ -2,9 +2,10 @@
 import '../../node_modules/jquery/src/jquery.js'
 import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import { renderRoot } from './components/about.js';
-
 import React from 'react';
 import { render } from 'react-dom';
+import Analytics from './components/analytics.js';
+Analytics.analytics();
 
 var root = document.getElementById('root');
 if (root !== null) {
--- src/client/terms-of-use.js
+++ src/client/terms-of-use.js
@@ -1,10 +1,10 @@
-// js dependencies
 import '../../node_modules/jquery/src/jquery.js'
 import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import { renderRoot } from './components/terms-of-use.js';
-
 import React from 'react';
 import { render } from 'react-dom';
+import Analytics from './components/analytics.js';
+Analytics.analytics();
 
 var root = document.getElementById('root');
 if (root !== null) {
