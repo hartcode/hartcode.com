f1c2067cac983c285d9d3451ea784dbc7fc0132f
Alex Hart <alex@hartcode.com>
2017-01-30 14:50:09 -0600

    Updated founder info

--- src/client/components/about.js
+++ src/client/components/about.js
@@ -20,13 +20,13 @@ export function renderRoot() {
         <h3>Alexander Hart</h3>
         <SocialMenu facebook={facebook} twitter={twitter} github={github} />
         <p>
-          Alex started programming at an early age. He was in middle school when he discovered basic code in the back of a PC Kids magazine. From there Alex started writing his own programs and games in QBasic. From an early age he loved computers and then taught himself C and C++. He went off to college to get a bachelor of arts degree in computer science with a minor in art. Since then he has worked as a developer for 11 years in the private sector.
+          Alex started programming at an early age. He was in middle school when he discovered Basic code in the back of a PC Kids magazine. From there Alex started writing his own programs and games in QBasic. From an early age he loved computers and then taught himself C and C++. He went off to college to get a bachelor of arts degree in computer science with a minor in art. Since then he has worked as a developer for 11 years in the private sector.
         </p>
         <p>
-          He got into programming computers as a way to make video games, and now he is following his dream.
+          He got into programming computers as a way to make video games, and now he is following his dream. Alex is commited to making fun and exciting experiences through the medium of video games.
         </p>
         <p>
-          Alex has always been a maker, and loves to create things, programs, and food for people.
+          Alex has always been a maker and loves to create games, software, art, soap, and scones.
         </p>
       </div>
       <Footer/>
