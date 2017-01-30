c683bc9c1e3887189659a36eb13e7acc42834aaf
Alex Hart <alex@hartcode.com>
2017-01-30 14:34:29 -0600

    Remove Game Module references
    Also remove privacy and terms links from footer

--- src/client/components/footer.js
+++ src/client/components/footer.js
@@ -12,8 +12,6 @@ export default class Footer extends React.Component {
           <li><a href="/">Home</a></li>
           <li><a href="/careers">Careers</a></li>
           <li><a href="/contact">Contact Us</a></li>
-          <li><a href="/privacy">Privacy</a></li>
-          <li><a href="/terms-of-use">Terms</a></li>
         </ul>
         <SocialMenu facebook={facebook} twitter={twitter} github={github} />
       </div>
