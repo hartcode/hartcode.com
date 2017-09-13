9831417c633df7ad2cb206fa4e0fe93171427a84
Alex Hart <alex@hartcode.com>
2017-09-13 09:33:19 -0500

    Remove everything
    Start Fresh

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM mhart/alpine-node:7.5
-RUN npm install --global yarn
-RUN mkdir /root/hartcode.com
-COPY . /root/hartcode.com/
-WORKDIR /root/hartcode.com
-RUN yarn
-RUN yarn run build
-EXPOSE 8811
-ENTRYPOINT ["yarn", "start"]
--- src/client/components/menu/social/social-menu-item.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default class SocialMenuItem extends React.Component {
-  render () {
-    return (
-          <a href={ this.props.url }><img className="socialMenuItem" src={this.props.image} /></a>
-    )
-  }
-}
