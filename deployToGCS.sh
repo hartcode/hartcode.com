b52400ed7a7f8638d789ce9b94ac00b3e4b0b258
Alex Hart <hartalex@gmail.com>
2017-09-29 16:45:16 +0000

    fix for deploy to gcs not working on test branch

--- deployToGCS.sh
+++ deployToGCS.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == 'prod' ] || [ $TRAVIS_BRANCH == $TRAVIS_TAG ]; then
-  if [ ! -d ${HOME}/google-cloud-sdk ]; then
-    rm -rf $HOME/google-cloud-sdk;
-    export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
-    curl https://sdk.cloud.google.com | bash;
-  fi;
-  gcloud auth activate-service-account --key-file client-secret.json;
-  docker build --build-arg COMMIT=$TRAVIS_COMMIT --build-arg TAG=$TRAVIS_BRANCH -t gcr.io/hartonline-cloud/$1:$TRAVIS_BRANCH-$TRAVIS_COMMIT .;
-  gcloud docker -- push gcr.io/hartonline-cloud/$1:$TRAVIS_BRANCH-$TRAVIS_COMMIT;
-else
-  echo "Branch not allowed to push to cloud repo"
-fi
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+  rm -rf $HOME/google-cloud-sdk;
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
+  curl https://sdk.cloud.google.com | bash;
+fi;
+gcloud auth activate-service-account --key-file client-secret.json;
+docker build --build-arg COMMIT=$TRAVIS_COMMIT --build-arg TAG=$TRAVIS_BRANCH -t gcr.io/hartonline-cloud/$1:$TRAVIS_BRANCH-$TRAVIS_COMMIT .;
+gcloud docker -- push gcr.io/hartonline-cloud/$1:$TRAVIS_BRANCH-$TRAVIS_COMMIT;
