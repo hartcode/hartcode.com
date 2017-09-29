e59e355cd4e7194a3aa36ff9461df72a155da5d8
Alex Hart <hartalex@gmail.com>
2017-09-29 16:06:53 +0000

    Update ports

--- deployAnsible.sh
+++ deployAnsible.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ $TRAVIS_BRANCH == 'prod' ] || [ $TRAVIS_BRANCH == $TRAVIS_TAG ]; then
-  export ANSIBLE_HOST_KEY_CHECKING=False
-  openssl dgst -md5 ansible
-  openssl dgst -md5 ansible.pub
-  chmod 400 ansible
-  ssh-add ansible
-  ansible-playbook -i inventory.yml .ansible-deploy.yml
-else
-  echo "Branch not allowed to deploy with Ansible"
+  export PORT=8811
 fi
+if [ $TRAVIS_BRANCH == 'test' ] || [ $TRAVIS_BRANCH == $TRAVIS_TAG ]; then
+  export PORT=8711
+fi
+export ANSIBLE_HOST_KEY_CHECKING=False
+openssl dgst -md5 ansible
+openssl dgst -md5 ansible.pub
+chmod 400 ansible
+ssh-add ansible
+ansible-playbook -i inventory.yml .ansible-deploy.yml
