4bade10b591768ea84e99d3e44ef026027253467
Alex Hart <alex@hartcode.com>
2018-03-21 11:20:24 -0500

    Update update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+openssl aes-256-cbc -K $encrypted_3b5afcaadad7_key -iv $encrypted_3b5afcaadad7_iv -in secrets.tar.enc -out secrets.tar -d
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+tar xvf secrets.tar
+chmod 400 travis
+eval "$(ssh-agent -s)"
+ssh-add travis
+git remote add deploy git@github.com:hartcode/hartcode.com.git
+git pull deploy HEAD:master
 git merge master
 ./updateDepends.sh
 npm run lint
