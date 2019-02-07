a8dee6c4058d3f518a0756304ca6380c8243cca9
Alex Hart <alex@hartcode.com>
2019-02-06 20:33:00 -0600

    fix lint issues

--- src/client/components/about.js
+++ src/client/components/about.js
@@ -5,12 +5,12 @@ import alex from '../images/alex.png'
 import SocialMenu from './menu/social/social-menu.js'
 
 export function renderRoot () {
-  var github = {username: 'hartalex', dark: 'true'}
-  var facebook = {username: 'alexander.hart7'}
-  var twitter = {username: 'hartalex0'}
+  var github = { username: 'hartalex', dark: 'true' }
+  var facebook = { username: 'alexander.hart7' }
+  var twitter = { username: 'hartalex0' }
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="body">
         <h1>About Us</h1>
         <p> We are small game studio located in the very heart of the midwest.</p>
@@ -20,16 +20,19 @@ export function renderRoot () {
         <h3>Alexander Hart</h3>
         <SocialMenu facebook={facebook} twitter={twitter} github={github} />
         <p>
-          Alex started programming at an early age. He was in middle school when he discovered Basic code in the back of a PC Kids magazine. From there Alex started writing his own programs and games in QBasic. From an early age he loved computers and then taught himself C and C++. He went off to college to get a bachelor of arts degree in computer science with a minor in art. Since then he has worked as a developer for 11 years in the private sector.
+          Alex started programming at an early age. He was in middle school when he discovered Basic code in the back of
+          a PC Kids magazine. From there Alex started writing his own programs and games in QBasic. From an early age he
+          loved computers and then taught himself C and C++. He went off to college to get a bachelor of arts degree in
+          computer science with a minor in art. Since then he has worked as a developer for 11 years in the private
+          sector.
         </p>
         <p>
-          He got into programming computers as a way to make video games, and now he is following his dream. Alex is commited to making fun and exciting experiences through the medium of video games.
+          He got into programming computers as a way to make video games, and now he is following his dream. Alex is
+          commited to making fun and exciting experiences through the medium of video games.
         </p>
-        <p>
-          Alex has always been a maker and loves to create games, software, art, soap, and scones.
-        </p>
+        <p>Alex has always been a maker and loves to create games, software, art, soap, and scones.</p>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- src/server/routes/info.js
+++ src/server/routes/info.js
@@ -1,3 +1,3 @@
 export default function (req, res) {
-  res.json({commit: process.env.COMMIT, tag: process.env.TAG})
+  res.json({ commit: process.env.COMMIT, tag: process.env.TAG })
 }
