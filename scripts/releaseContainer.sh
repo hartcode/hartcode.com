4fa8eaffa2b159a6e3727d7f56f0bfe702cdf79c
Alex Hart <hartalex@gmail.com>
2019-02-10 19:27:46 -0600

    build(Remove Unnecessary build steps):

--- scripts/releaseContainer.sh
+++ scripts/releaseContainer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-npm run build
-pip install ansible --user
 openssl aes-256-cbc -K $encrypted_3b5afcaadad7_key -iv $encrypted_3b5afcaadad7_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
 
