996a0d10f536077f7e75e63181f729b4ae7bf8df
Alex Hart <hartalex@gmail.com>
2019-02-10 20:08:50 -0600

    build: Remove branch name from container versions
    Also added rollout status check to the end of releaseContainer.sh

--- scripts/buildContainer.sh
+++ scripts/buildContainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm run build
-docker build --build-arg COMMIT=$TRAVIS_COMMIT --build-arg TAG=$TRAVIS_BRANCH -t gcr.io/hartonline-cloud/hartcode.com:$TRAVIS_BRANCH-$TRAVIS_COMMIT .;
-gcloud docker -- push gcr.io/hartonline-cloud/hartcode.com:$TRAVIS_BRANCH-$TRAVIS_COMMIT;
+docker build --build-arg COMMIT=$TRAVIS_COMMIT -t gcr.io/hartonline-cloud/hartcode.com:$TRAVIS_COMMIT .;
+gcloud docker -- push gcr.io/hartonline-cloud/hartcode.com:$TRAVIS_COMMIT;
--- scripts/releaseContainer.sh
+++ scripts/releaseContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sed -i "s/(TRAVIS_BRANCH)/$TRAVIS_BRANCH/g" kube.conf.json
 sed -i "s/(TRAVIS_COMMIT)/$TRAVIS_COMMIT/g" kube.conf.json
 ./kubectl apply -f kube.conf.json
+./kubectl rollout status deployment.v1.apps/hartcode
