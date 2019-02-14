04d2e509732ee4a64e28a24da401820b8b6bf2cb
Alex Hart <hartalex@gmail.com>
2019-02-13 22:13:58 -0600

    feat(careers.js): Fetch career data from the api

--- src/client/components/careers.js
+++ src/client/components/careers.js
@@ -3,16 +3,40 @@ import Header from './header.js'
 import Footer from './footer.js'
 
 export function renderRoot () {
-  return (
-    <div>
-      <Header/>
-      <div className="body">
-        <h1>Careers</h1>
-        <p>
-          We have no employment opportunities at this time.
-        </p>
+  return <Careers />
+}
+export default class Careers extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      data: '...'
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://api.test.hartcode.com/careers')
+      .then((response) => {
+        return response.json()
+      })
+      .then((res) => {
+        const data = res.data
+        console.log(data)
+        this.setState({
+          data
+        })
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <div className="body">
+          <h1>Careers</h1>
+          <p>{this.state.data}</p>
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
-  )
+    )
+  }
 }
