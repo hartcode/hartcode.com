5cae3223a75c06798a531e1e32eb381a0cc1bc8c
Alex Hart <hartalex@gmail.com>
2019-12-30 14:24:10 -0600

    fix(prettier/eslint): correct eslint vs prettier issues

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -2,17 +2,25 @@ import React from 'react'
 import Header from './header.js'
 import Footer from './footer.js'
 
-export function renderRoot () {
+export function renderRoot() {
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="body">
         <div>
-          <a href="/products"><div className='products'><h1>Products</h1></div></a>
-          <a href="/hire"><div className='hire'><h1>Hire us</h1></div></a>
+          <a href="/products">
+            <div className="products">
+              <h1>Products</h1>
+            </div>
+          </a>
+          <a href="/hire">
+            <div className="hire">
+              <h1>Hire us</h1>
+            </div>
+          </a>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
